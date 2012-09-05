bf51522c249fc4fe966d26a5372aa8cdee93876f
mkallberg <morten.kallberg@gmail.com>
2012-09-05 11:25:05 -0700

    hgh

---
+++ src/BaseSpacePy/model/Application.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+class Application:
+
+    def __init__(self):
+        self.swaggerTypes = {
+            'Id': 'str',
+            'Href': 'str',
+            "Name":"str",
+            "HomepageUri":"str",
+            "ShortDescription":"str",
+            "DateCreated":"str"            
+        }
+
+        self.Id                   = None
+        self.Href                 = None
+        self.Name                 = None
+        self.HomepageUri          = None
+        self.ShortDescription     = None
+        self.DateCreated          = None
+        
+
