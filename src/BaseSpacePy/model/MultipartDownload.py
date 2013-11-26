74be09fa67e368c14f676c922c6bffee320d7514
Eric Smith <esmith@illumina.com>
2013-11-25 17:10:13 -0800

    Replaced has_finished() with join(), fixed bug where tasks left in queue block new get() calls by workers; full stats no longer showing with verbose mode

--- src/BaseSpacePy/model/MultipartDownload.py
+++ src/BaseSpacePy/model/MultipartDownload.py
@@ -20,6 +20,7 @@ import multiprocessing
 import Queue
 import shutil
 import sys
+import signal
 
 class DownloadTask(object):
     def __init__(self, api, bs_file_id, file_name, local_path, piece, part_size, total_size, attempt, temp_dir):
@@ -51,12 +52,13 @@ class DownloadTask(object):
             endbyte = self.total_size - 1
         try:
             self.api.fileDownload(self.bs_file_id, self.local_path, transFile, [startbyte, endbyte])
+            #raise Exception("TESTING")
         except Exception as e:
             self.success = False
             self.err_msg = str(e)
             # TODO clean up partially downloaded file?
         else:
-            # TODO test that downloaded file is correct size
+            # test that downloaded file is correct size
             if os.path.getsize(os.path.join(self.local_path, transFile)) != endbyte - startbyte + 1:
                 self.success = False
                 self.err_msg = "ERROR - downloaded file chunk has incorrect size, for task: " + str(self)
@@ -70,65 +72,86 @@ class DownloadTask(object):
     
 class Consumer(multiprocessing.Process):
     
-    def __init__(self, task_queue, result_queue, pauseEvent, haltEvent):
+    def __init__(self, task_queue, result_queue, halt_event, verbose):    
         multiprocessing.Process.__init__(self)
         self.task_queue = task_queue
-        self.result_queue = result_queue
-        self.pause = pauseEvent
-        self.halt  = haltEvent
-       # self.timeOutTask = 60   # seconds for a task to timeout
+        self.result_queue = result_queue        
+        self.halt  = halt_event
+        self.verbose = verbose
+        self.task_timeout = 5 # secs
         
-        self.retries = 1
+        self.retries = 10
         
     def run(self):
         '''
-        Executes tasks from the task queue until poison pill is reached or halt signal found; has pause capability
+        Executes tasks from the task queue until poison pill is reached, halt 
+        signal is found, or something went wrong such as a timeout when getting
+        new tasks (or task queue empty when shouldn't be).
+        
+        Retries failed tasks, and add task results to result_queue.
+        When a task fails for all retries, set halt signal to alert other workers
+        and purge task queue or remaining tasks (to unblock join() in parent process)
+        
+        Turn off SIGINT (Ctrl C), handle in parent process
         '''
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
         while True:
-            # get next task unless paused
-            if not self.pause.is_set(): 
-                next_task = self.task_queue.get()
-            print "    processing task: " + str(next_task)
-            # poison pill means we're at the end of the task queue                
-            if next_task is None:            
-                print 'Worker halting, found final task for worker %s' % self.name
-                #self.task_queue.task_done()
-                break                
-            # task queue empty -- shouldn't need this due to poison pill -- may block if queue somehow gets empty?
-            elif self.task_queue.qsize()==0:
-                print 'Worker halting, found task queue empty for worker %s' % self.name
-                #self.task_queue.task_done()
+            if self.halt.is_set():
+                print 'Worker exiting, found halt signal for worker %s' % self.name
+                # purge queue in case halt was signaled from outside a worker
+                self.purge_task_queue()            
+                break                         
+            try:
+                next_task = self.task_queue.get(True, self.task_timeout) # block until timeout
+            except Queue.Empty:
+                print 'Worker exiting, getting task from task queue timed out and/or is empty'
                 break
-            elif self.halt.is_set():
-                print 'Worker halting, found halt signal for worker %s' % self.name
-                #self.task_queue.task_done() # TODO hmmm, didn't process task but marked it as done...?
-                break #sys.exit()                                            
-            elif self.pause.is_set():                   
-                time.sleep(3)                                       
+            if self.verbose:
+                print "Processing task: " + str(next_task)
+            if next_task is None:            
+                print 'Worker exiting, found final task for worker %s' % self.name
+                self.task_queue.task_done()
+                break                                                                   
             else:                                                       
                 # attempt to run tasks, with retry
+                # TODO add progressively longer retries?
                 for i in xrange(1, self.retries + 1):    
                     answer = next_task()                    
                     if answer.success == True:
-                        #self.task_queue.task_done()                   
-                        self.result_queue.put(answer)
+                        self.task_queue.task_done()                   
+                        self.result_queue.put(True)
                         break
                     else:
                         print "Worker retrying task " + str(next_task) + " after failure, retry attempt " + str(i) + " with error msg: " + answer.err_msg
                 if not answer.success == True:
-                    print "Worker halting, too many failures with retry for worker %s" % str(self)        
-                    # add failed answer to result queue and set halt event for all workers
-                    #self.task_queue.task_done()                   
-                    self.result_queue.put(answer)                    
+                    print "Worker exiting, too many failures with retry for worker %s" % str(self)        
+                    self.task_queue.task_done()                   
+                    self.result_queue.put(False)
+                    self.purge_task_queue() # purge task queue in case there's only one worker                    
                     self.halt.set()
                     break
-                    #sys.exit(1)
         print "Worker exiting, worker %s" % self.name
         return
-
+    
+    def purge_task_queue(self):
+        '''
+        Purge all remaining tasks from task queue. This will also remove poison pills
+        (final tasks), so run() must handle an empty queue (by using a timeout with
+        task_queue.get() ).
+        '''
+        if self.verbose:
+            print "purging task queue"
+        while 1:
+            try:
+                self.task_queue.get(False)                
+            except Queue.Empty:            
+                break
+            else:
+                self.task_queue.task_done()
+                
 class Executor(object):
     '''
-    Multi-processing task manager, with task queue, pause and halt methods, and callback to finalize once workers are completed
+    Multi-processing task manager, with task queue, and callback to finalize once workers are completed
     '''
     def __init__(self, verbose=0):
         self.verbose = verbose
@@ -137,11 +160,9 @@ class Executor(object):
         self.zeroQCount = 0
         self.start_time = -1
         
-        # Establish communication and task queues, create consumers
-        self.tasks = multiprocessing.Queue() # TODO JoinableQueue()?
-        self.completed_pool = multiprocessing.Queue()                        
-        self.pauseEvent = multiprocessing.Event()
-        self.haltEvent = multiprocessing.Event()
+        self.tasks = multiprocessing.JoinableQueue()
+        self.result_queue = multiprocessing.Queue()                        
+        self.halt_event = multiprocessing.Event()
     
     def add_task(self, task):
         self.tasks.put(task)
@@ -157,7 +178,7 @@ class Executor(object):
             return self.tasks.qsize() - len(self.consumers)
     
     def add_workers(self, num_workers):
-        self.consumers = [ Consumer(self.tasks, self.completed_pool, self.pauseEvent, self.haltEvent) for i in xrange(num_workers) ]
+        self.consumers = [ Consumer(self.tasks, self.result_queue, self.halt_event, self.verbose) for i in xrange(num_workers) ]
         for c in self.consumers:
             self.tasks.put(None)   # add poison pill
 
@@ -173,33 +194,33 @@ class Executor(object):
         else: 
             return time.time() - self.start_time
     
-    def has_finished(self):
-        '''
-        If the task queue is empty, halt all workers
-        Return the count of available workers
-        '''
-        if self.status == 'Initialized': 
-            return 0            
-        # TODO temporary hack, check if the queue is empty        
-        if self.tasks.qsize()==0:
-            self.zeroQCount +=1
-        if self.zeroQCount>15:
-            self.halt_workers()
-        
-        return not self.get_running_worker_count()>0
+    #def has_finished(self):
+    #    '''
+    #    If the task queue is empty, halt all workers
+    #    Return the count of available workers
+    #    '''
+    #    if self.status == 'Initialized': 
+    #        return 0            
+    #    # TODO temporary hack, check if the queue is empty        
+    #    if self.tasks.qsize()==0:
+    #        self.zeroQCount +=1
+    #    if self.zeroQCount>15:
+    #        self.halt_workers()
+    #    
+    #    return not self.get_running_worker_count()>0
     
 #    def pauseWorkers(self):
 #        self.pauseEvent.set()
 #        self.status = 'Paused'
     
-    def halt_workers(self):
-        for c in self.consumers: 
-            c.terminate()          
-            # TODO use terminate here or set halt signal?      
+    #def halt_workers(self):
+    #    for c in self.consumers: 
+    #        c.terminate()          
+    #        # TODO use terminate here or set halt signal?      
 
     def start_workers(self, status_callback, finalize_callback, test_interval=5):
         '''
-        Start workers, handle paused and other states, wait until workers finish to clean up small file downloads
+        Start workers, wait until workers finish to clean up small file downloads
         '''
         if self.status=='Terminated' or self.status=='Completed':
             raise Exception('Cannot resume a ' + self.status + ' session.')
@@ -207,39 +228,46 @@ class Executor(object):
         if self.status == 'Initialized':
             self.start_time = time.time()
             for w in self.consumers:
-                w.start()
-        if self.status == 'Paused':
-            self.pauseEvent.clear()
+                w.start()        
         self.status = 'Running'
         
         # wait until tasks queues are empty, then call finalize callback if all went well
-        # TODO use join() method instead of has_finished()
-        i=0
-        while not self.has_finished():            
-            if self.verbose and i: print str(i) + ': ' + status_callback()
-            time.sleep(test_interval)
-            i+=1
-        print "***Workers finished***" # TODO TEMP
+        if self.verbose:
+            print "Workers started"
+        try:
+            self.tasks.join()
+        except (KeyboardInterrupt, SystemExit):
+            self.result_queue.put(False)
+            self.halt_event.set()
+            if self.verbose:
+                print "Halting all workers -- received exit signal"
+        if self.verbose:
+            print "Workers finished"
+        #self.halt_workers()
         
-        if self.get_running_worker_count():
-            raise Exception('Error - trying to finalize scheduler when workers are still running.')
+        #i=0
+        #while not self.has_finished():            
+        #    if self.verbose and i: print str(i) + ': ' + status_callback()
+        #    time.sleep(test_interval)
+        #    i+=1
+        #print "***Workers finished***" # TODO TEMP
+        
+        #if self.get_running_worker_count():
+        #    raise Exception('Error - trying to finalize scheduler when workers are still running.')
         if self.status=='Running':
             # check that all workers completed successfully (first sleep to make sure we're ready)
             time.sleep(1)            
             finalize = True
             while 1:
                 try:
-                    answer = self.completed_pool.get(False)
-                    print "getting worker answer"
-                except Queue.Empty:
-                    print "completed pool empty, breaking (hopefully exiting)"
+                    success = self.result_queue.get(False) # non-blocking                    
+                except Queue.Empty:                    
                     break
                 else:                    
-                    if answer.success == False:
-                        print "answer failed"
-                        finalize = False
-                    else:
-                        print "answer succeeded"                    
+                    if success == False:
+                        if self.verbose:
+                            print "Found a failed task -- won't call finalize callback"
+                        finalize = False                                
             #for w in self.consumers:
             #    if w.exitcode is None:
             #        raise Exception('Error - at least one process is active when all processes should be terminated')
@@ -250,17 +278,7 @@ class Executor(object):
                 finalize_callback()                                            
             self.status = 'Completed'
         else:
-            raise Exception('To finalize, the status of the transfer must be "Running."')
-        
-        #TODO TEST empty task queue
-        print "purging task queue"
-        while 1:
-            try:
-                answer = self.tasks.get(False)                
-            except Queue.Empty:
-                print "completed task queue empty, breaking (hopefully exiting)"
-                break
-        print "DONE with start_workers"
+            raise Exception('To finalize, the status of the transfer must be "Running."')                
 
 class MultipartDownload(object):
     '''
@@ -276,6 +294,7 @@ class MultipartDownload(object):
         self.temp_dir       = temp_dir
         self.start_chunk    = start_chunk        
         self.setup()
+        self.start_download()
     
     def __str__(self):
         return "MPU - Status: " + self.exe.status +  \
@@ -303,7 +322,7 @@ class MultipartDownload(object):
             t = DownloadTask(self.api, self.fileId, self.file_name, self.local_path, i, self.part_size, total_size, 0, self.temp_dir)
             self.exe.add_task(t)            
         self.exe.add_workers(self.process_count)
-        self.task_total = self.exe.get_task_count()
+        self.task_total = self.exe.get_task_count()                
                         
         if self.verbose: 
             print "Total File Size " + str(total_size) + " bytes"
@@ -319,8 +338,8 @@ class MultipartDownload(object):
         '''
         status_callback = self.status_update_msg
         finalize_callback = self.combine_file_chunks
-        self.exe.start_workers(status_callback, finalize_callback, test_interval)       
-        print "DONE DONE with start_download" 
+        self.exe.start_workers(status_callback, finalize_callback, test_interval)
+        self.counter.terminate()         
     
     def status_update_msg(self):
         '''
